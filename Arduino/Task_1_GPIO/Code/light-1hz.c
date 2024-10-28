cec8b49d8962e0e73b2fb0dbcc97fb1aa6ad0572
Peak-Xu <3075664510@qq.com>
2024-10-28 22:29:14 +0800

    my work

--- Arduino/Task_1_GPIO/Code/light-1hz.c
+++ Arduino/Task_1_GPIO/Code/light-1hz.c
@@ -7,7 +7,7 @@ pinMode(13,output);//pinMode是Arduino提供的库，用于设置引脚输入输
 void loop() {
 digitalWrite(13,HIGH);//digitalWrite是Arduino提供的库，用于设置输出引脚的高低电平均状态
 delay(500);//delay是Arduino提供的库，用于设置占用单片机的时间，即延迟
-dugitalWrite(13,low);
+dugitalWrite(13,LOW);
 delay(500);
 }
 //steup和loop函数一定要有，空的也可以，不然会报错
